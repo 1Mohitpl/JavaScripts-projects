9cb9d1258e28def8b89b65219a36845b879266e2
Mohit kumar paul <mohit1paul@gmail.com>
2025-02-15 23:49:11 +0530

    add carousel

--- Carousel/main.js
+++ Carousel/main.js
@@ -35,19 +35,59 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
 prevButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current_carousel_slid');
     const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotnaves.querySelectorAll('.current_slid');
+    const prevDot = currentDot.previousElementSibling;
 
     moveToSlide(track, currentSlide, prevSlide);
+    updatesDots(currentDot, prevDot);
 } )
 
+const updatesDots = (currentDot, targetDot) =>{
+    currentDot.classList.remove('current_slid');
+    targetDot.classList.add('current_slid');
+}
 
 
 nextButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current_carousel_slid');
     const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotnaves.querySelectorAll('.current_slid');
+    const nextDot = currentDot.nextElementSibling;
+
     
     moveToSlide(track, currentSlide, nextSlide);
     // movbe to the next slide
-
+    updatesDots(currentDot, nextDot);
+    
     
 })
 // when i clike the nav indicator then also move to the slide
+
+dotnaves.addEventListener('click', e =>{
+     const targetDot = e.target.closest('button');
+
+     if(!targetDot) return;
+
+     const currentSlide = track.querySelector('.current_slid');
+     const currentDot = dotnaves.querySelector('.current_slid');
+     const targetIndex = dots.findIndex(dot => dot === targetDot);
+     const targetSlide = slides[targetIndex] // to get each slide with each index
+
+     moveToSlide(track, currentSlide, targetSlide);
+
+
+     updatesDots(currentDot, targetDot);
+
+     if(targetIndex === 0){
+        prevButton.classList.add('is_hidden');
+        nextButton.classList.remove('is_hidden');
+    
+     } else if(targetIndex === slides.length-1){
+        prevButton.classList.remove('is_hidden');
+        nextButton.classList.add('is_hidden');
+     } else {
+        prevButton.classList.remove('is_hidden');
+        nextButton.classList.remove('is_hideen');
+     }
+     
+})
